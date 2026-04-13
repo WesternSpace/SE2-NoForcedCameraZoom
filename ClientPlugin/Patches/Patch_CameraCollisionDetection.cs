4da286e4853d628dc9da37dd1212efa81e7333f8
WesternSpace <80211714+WesternSpace@users.noreply.github.com>
2026-04-13 05:13:17 -0400

    Initial project.

---
+++ ClientPlugin/Patches/Patch_CameraCollisionDetection.cs
@@ -0,0 +1,10 @@
+﻿using HarmonyLib;
+using Keen.Game2.Client.GameSystems.CameraSystems.Modes;
+
+namespace ClientPlugin.Patches;
+
+[HarmonyPatch(typeof(ThirdPersonCameraComponent), "CameraCollisionDetection")]
+internal class Patch_CameraCollisionDetection
+{
+    private static bool Prefix() => false;
+}
--- ClientPlugin/Plugin.cs
+++ ClientPlugin/Plugin.cs
@@ -7,28 +7,15 @@ namespace ClientPlugin;
 
 public class Plugin : IPlugin
 {
-    public const string Name = "ClientPluginTemplate";
-    public static Plugin Instance;
-    
-    // The data directory will be provided by a proper SDK in the future.
-    // This static function is currently injected by Pulsar, which will
-    // remain compatible, even after the SDK's release.
-    private static Func<string, string, string> GetConfigPath;
-    public string DataDir { get; private set; } = GetConfigPath(Name, null);
-
-    public Config Config;
+    public const string Name = "SE2-NoForcedCameraZoom";
     
     public Plugin()
     {
-        Instance = this;
-        
-        Config = ConfigStorage.Load();
-        
         Log.Default.WriteLine($"[{Name}] Loaded plugin.");
 #if DEBUG
         Harmony.DEBUG = true;
 #endif
-        Harmony harmony = new Harmony(Name);
+        Harmony harmony = new(Name);
         harmony.PatchAll(Assembly.GetExecutingAssembly());
         Log.Default.WriteLine($"[{Name}] Applied patches");
     }
